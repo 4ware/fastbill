07426c49dc4afc23aaf1908de9c16f8ba082cc18
Kai Wernicke <kai@4ware.net>
2011-09-27 19:13:19 +0200

    initial commit

--- lib/fastbill.rb
+++ lib/fastbill.rb
@@ -3,7 +3,7 @@ require "fastbill/version"
 module Fastbill
   attr_reader :email, :api_key
   class << self
-    def new(email, api_key)
+    def authenticate(email, api_key)
       @email = email
       @api_key = api_key
       self
