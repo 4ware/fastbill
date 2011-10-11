67ad4995ba9cdd3783819c2bdbaa56f2d1b8687d
Kai Wernicke <kai@4ware.net>
2011-10-11 13:13:59 +0200

    update to invoice

--- lib/fastbill/invoice.rb
+++ lib/fastbill/invoice.rb
@@ -38,6 +38,7 @@ class Invoice
     
   end
   def hydrate(body)
+    #"INVOICE_ID"=>"72783", "INVOICE_TYPE"=>"outgoing", "CUSTOMER_ID"=>"62288", "CUSTOMER_COSTCENTER_ID"=>"0", "CURRENCY_CODE"=>"EUR", "TEMPLATE_ID"=>"3493", "INVOICE_NUMBER"=>"145", "INTROTEXT"=>"Guten Tag Herr Schlenker,\n\nanbei meine Rechnung über den Umbau der Young Gans Community auf das aktuelle Layout.\n\nGruß, Kai Wernicke", "PAYED_DATE"=>"0000-00-00 00:00:00", "IS_CANCELED"=>"0", "INVOICE_DATE"=>"2011-10-10", "DUE_DATE"=>"2011-10-24 09:54:09", "DELIVERY_DATE"=>nil, "SUB_TOTAL"=>"100", "VAT_TOTAL"=>"19", "VAT_ITEMS"=>{"VAT_ITEM"=>{"VAT_PERCENT"=>"19.00", "VAT_VALUE"=>"19"}}, "ITEMS"=>{"ITEM"=>{"INVOICE_ITEM_ID"=>"148279", "ARTICLE_NUMBER"=>nil, "DESCRIPTION"=>"Stunden / Programmierung", "QUANTITY"=>"5.00", "UNIT_PRICE"=>"20.0000", "VAT_PERCENT"=>"19.00", "VAT_VALUE"=>"19", "COMPLETE_NET"=>"100", "COMPLETE_GROSS"=>"119", "SORT_ORDER"=>"1"}}, "TOTAL"=>"119"
     @is_new = false
     @id = body["INVOICE_ID"]
     @invoice_type = body["INVOICE_TYPE"]
@@ -55,6 +56,7 @@ class Invoice
     @sub_total = body["SUB_TOTAL"]
     @vat_total = body["VAT_TOTAL"]
     @vat_items = []
+    @total = body["TOTAL"]
     for vat_item in body["VAT_ITEMS"].each
       @vat_items.push InvoiceVatItem.new vat_item.last
     end
@@ -64,7 +66,6 @@ class Invoice
       i.hydrate(item.last)
       @items.push i
     end
-    @total = body["TOTAl"]
   end
   def parse_date(date)
     if date != nil && date != "0000-00-00 00:00:00"
