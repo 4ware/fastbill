302a7b1938358ff0e19d38e34dd086e9c35f7667
Kai Wernicke <kai@4ware.net>
2011-10-18 10:19:14 +0200

    init testsuite

--- spec/fastbill_spec.rb
+++ spec/fastbill_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
 describe Fastbill do
+  #puts MY_CONFIG['test']
   #pending
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,8 @@ require 'bundler/setup'
 
 require 'fastbill' # and any other gems you need
 
+MY_CONFIG = YAML.load_file("#{File.dirname(__FILE__)}/support/fastbill.yml")
+
 RSpec.configure do |config|
   # some (optional) config here
 end
